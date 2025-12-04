f0729d332f82475a985d948f134339a04f7a1e58
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2025-12-04 16:39:55 +0300

    `Added imports for Pharmacy, Payments, Subscription, and Analytics modules`

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DrugsModule } from './drugs/drugs.module';
 import { SalesModule } from './sales/sales.module';
+import { PharmacyModule } from './pharmacy/pharmacy.module';
+import { PaymentsModule } from './payments/payments.module';
+import { SubscriptionModule } from './subscription/subscription.module';
+import { AnalyticsModule } from './analytics/analytics.module';
 
 @Module({
-  imports: [UsersModule, DrugsModule, SalesModule],
+  imports: [UsersModule, DrugsModule, SalesModule, PharmacyModule, PaymentsModule, SubscriptionModule, AnalyticsModule],
   controllers: [AppController],
   providers: [AppService],
 })
