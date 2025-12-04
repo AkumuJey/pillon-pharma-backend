db7df35f780d3f30d5dc6283903c7af33c4336a8
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2025-12-04 14:22:49 +0300

    `Update user entity and service to use userId instead of id`

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import {
+  IsEmail,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { UserRole } from '../dto/create-user.dto';
+
+export class User {
+  @IsNumber()
+  userId: number;
+  @IsString()
+  name: string;
+  @IsString()
+  @IsEmail()
+  email: string;
+  @IsString()
+  @IsOptional()
+  phone?: string;
+  @IsEnum(UserRole, { message: 'Invalid role' })
+  role: UserRole;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,29 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto, UserRole } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  private users: CreateUserDto[] = [
+  private users: User[] = [
     {
+      userId: 1,
       name: 'Alice Johnson',
       email: 'alice.johnson@example.com',
       phone: '+254712345001',
       role: UserRole.MANAGER,
     },
     {
+      userId: 2,
       name: 'Bob Mwangi',
       email: 'bob.mwangi@example.com',
       phone: '+254712345002',
       role: UserRole.SELLER,
     },
     {
+      userId: 3,
       name: 'Catherine Njeri',
       email: 'catherine.njeri@example.com',
       phone: '+254712345003',
       role: UserRole.SELLER,
     },
     {
+      userId: 4,
       name: 'David Otieno',
       email: 'david.otieno@example.com',
       phone: '+254712345004',
@@ -32,8 +37,13 @@ export class UsersService {
   ];
 
   create(createUserDto: CreateUserDto) {
-    this.users.push(createUserDto);
-    return createUserDto;
+    const newUser: User = {
+      userId: this.users.length + 1, // Always assign yourself
+      ...createUserDto,
+    };
+
+    this.users.push(newUser);
+    return newUser;
   }
 
   findAll() {
